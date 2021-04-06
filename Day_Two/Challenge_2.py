5a05b37af41f9f0d29b921798d7656b27eee9f15
ederperez95 <65266864+ederperez95@users.noreply.github.com>
2021-04-06 12:27:11 -0500

    Update Challenge_2.py

--- Day_Two/Challenge_2.py
+++ Day_Two/Challenge_2.py
@@ -9,7 +9,7 @@ Created on Thu Mar 18 17:23:15 2021
 
 import pandas as pd
 
-df = pd.read_csv(r"F:\Own_Projects\Challenges\Day_Two\Passwords.txt", sep = ' ', header = None)
+df = pd.read_csv(r"Passwords.txt", sep = ' ', header = None)
 df["max"] = df[0].str.split("-", n = 1, expand = True)[1]
 df["min"] = df[0].str.split("-", n = 1, expand = True)[0]
 df[1] = df[1].replace({':':''}, regex = True)
@@ -19,7 +19,7 @@ df[1] = df[1].replace({':':''}, regex = True)
 def Number_letters(string, letter):
     return sum(list(map(lambda x: 1 if letter == x else 0, string)))
 
-respuesta_1 = sum(list(map(lambda x: 1 if Number_letters(df[2][x], df[1][x]) <= float(df['max'][x]) and 
+Answer_1 = sum(list(map(lambda x: 1 if Number_letters(df[2][x], df[1][x]) <= float(df['max'][x]) and 
                          Number_letters(df[2][x], df[1][x]) >= float(df['min'][x]) else 0, df.index)))
 
 
@@ -35,5 +35,5 @@ def match(DataFrame, x):
         return 0
 
 
-respuesta_2 = sum(list(map(lambda x: 1 if match(df, x) == 1 else 0, df.index)))
+Answer_2 = sum(list(map(lambda x: 1 if match(df, x) == 1 else 0, df.index)))
     
